1561f67803d69e45c5b35cc9331b8adef17e40b2
ITCINFOTECH\23702 <vinay.jacob@itcinfotech.com>
2016-08-24 17:20:21 +0530

    All files and folders of magento including Sheamoisture Theme

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_jvnc',
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 23 Aug 2016 04:54:35 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'ac1fb6a41bf43aaa4e9b3e33b5242999',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'sheamoisture2',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+    'compiled_config' => 1,
+  ),
+);
